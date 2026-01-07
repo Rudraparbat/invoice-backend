0a01cb7cd3106e384d64d3bbcb06462111b6ce4c
Ruddy <163815002+Rudraparbat@users.noreply.github.com>
2026-01-07 21:51:30 +0530

    Enable CORS and add middleware for handling it

--- invoice_generator/settings.py
+++ invoice_generator/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 CSRF_TRUSTED_ORIGINS = [
     "https://invoice-backend-ttkj.onrender.com",
     "http://localhost:3000",
@@ -68,6 +68,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
